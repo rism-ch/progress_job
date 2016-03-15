7d61bc8d85a40442b21b2493bc32b4da5355cf4a
Rodolfo Zitellini <rwz@xhero.org>
2016-03-15 15:54:31 +0100

    Rescue exception

--- app/controllers/progress_job/progress_controller.rb
+++ app/controllers/progress_job/progress_controller.rb
@@ -2,9 +2,14 @@ module ProgressJob
   class ProgressController < ActionController::Base
 
     def show
-      @delayed_job = Delayed::Job.find(params[:job_id])
-      percentage = !@delayed_job.progress_max.zero? ? @delayed_job.progress_current / @delayed_job.progress_max.to_f * 100 : 0
-      render json: @delayed_job.attributes.merge!(percentage: percentage).to_json
+      begin
+        @delayed_job = Delayed::Job.find(params[:job_id])
+        percentage = !@delayed_job.progress_max.zero? ? @delayed_job.progress_current / @delayed_job.progress_max.to_f * 100 : 0
+        render json: @delayed_job.attributes.merge!(percentage: percentage).to_json
+      rescue
+        render json: 'none'.to_json
+      end
+
     end
 
   end
