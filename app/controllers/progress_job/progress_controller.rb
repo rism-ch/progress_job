b085abb0983eef6b93e30ad4cc2d4fe896d47d32
Rodolfo Zitellini <rwz@xhero.org>
2016-03-16 13:49:57 +0100

    Avoid division by 0

--- app/controllers/progress_job/progress_controller.rb
+++ app/controllers/progress_job/progress_controller.rb
@@ -4,7 +4,10 @@ module ProgressJob
     def show
       begin
         @delayed_job = Delayed::Job.find(params[:job_id])
-        percentage = !@delayed_job.progress_max.zero? ? @delayed_job.progress_current / @delayed_job.progress_max.to_f * 100 : 0
+        percentage = -1
+        if @delayed_job.progress_max && @delayed_job.progress_current
+          percentage = !@delayed_job.progress_max.zero? ? @delayed_job.progress_current / @delayed_job.progress_max.to_f * 100 : 0
+        end
         render json: @delayed_job.attributes.merge!(percentage: percentage).to_json
       rescue
         render json: 'none'.to_json
